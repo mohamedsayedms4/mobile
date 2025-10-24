b9db75d39582a25e934265ed38580bcb626f602c
mohamedsayedms4 <mohamedms4002@gmail.com>
2025-10-24 15:20:30 +0300

    cors

--- src/main/java/org/example/mobily/controller/MobileBrandDetailsController.java
+++ src/main/java/org/example/mobily/controller/MobileBrandDetailsController.java
@@ -11,6 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/v1/brands")
+@CrossOrigin("*")
 @RequiredArgsConstructor
 public class MobileBrandDetailsController {
 
